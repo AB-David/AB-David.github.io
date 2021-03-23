4c232511bc8b862953703a0fadf2248f34b7fb0a
David Abadi <GL.DAVID.GL@GMAIL.COM>
2021-03-23 12:42:20 -0400

    fixed console issues, replaced typed animation

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 100;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+
 let projects = [
     {
         repo:"Real-Time-Bus-Tracker",
@@ -12,10 +53,10 @@ let projects = [
     {
         repo:"Pacman-factory",
         image:"./assets/img/pacmen factory.gif",
-        desc:"This will create any number of pacman as a factory, then bounce them with an option to change their velocity."
+        desc:"This will create any number of pacman as a factory, then sends them all over the display with an option to increase their velocity."
     },
     {
-        repo:"Ball-factory",
+        repo:"Gravity-Ball-factory",
         image:"./assets/img/ball_factory.gif",
         desc:"This will create any number of balls in different colors, then bounce them within a defined area while taking gravity into consideration."
     },
@@ -27,7 +68,23 @@ let projects = [
 ];
 
 window.onload = ()=>{
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+
+
     let ul = document.getElementById('projects-dropdown')
+    
     for (i=0; i<projects.length; i++) {
         let li = document.createElement('li')
         let a = document.createElement('a')
@@ -36,54 +93,56 @@ window.onload = ()=>{
         a.innerHTML = projects[i].repo;
         li.appendChild(a)
         ul.appendChild(li)
-        console.log('created element' + li)
     }
 
     let container = document.getElementById('projects-grid')
-    for (i=0; i<projects.length; i++) {
-        let div = document.createElement('div')
-        div.classList.add('projects-grid-item')
-        let cardShadowDiv = document.createElement('div')
-        cardShadowDiv.classList.add('card','shadow-sm')
-        div.appendChild(cardShadowDiv)
-        let cardImg = document.createElement('img')
-        cardImg.src = presentImage(projects[i].image)
-
-        let aImgLink = document.createElement('a')
-        aImgLink.href = demoAddress(projects[i].repo)
-        aImgLink.appendChild(cardImg)
-
-        cardShadowDiv.appendChild(aImgLink)
-        let cardBody = document.createElement('div')
-        cardBody.classList.add('card-body')
-        cardShadowDiv.appendChild(cardBody)
-        let h4 = document.createElement('h4')
-        h4.classList.add('capitalize')
-        h4.innerHTML = presentName(projects[i].repo)
-        cardBody.appendChild(h4)
-        let p = document.createElement('p')
-        p.classList.add('card-text')
-        p.innerHTML = projects[i].desc
-        cardBody.appendChild(p)
-        let flexDiv = document.createElement('div')
-        flexDiv.classList.add('d-flex','justify-content-between','align-items-center')
-        cardBody.appendChild(flexDiv)
-        let btnDiv = document.createElement('div')
-        btnDiv.classList.add('btn-group')
-        flexDiv.appendChild(btnDiv)
-        let aDemo = document.createElement('a')
-        aDemo.classList.add('btn','btn-sm','btn-outline-secondary')
-        aDemo.href = demoAddress(projects[i].repo)
-        aDemo.innerHTML = 'Demo';
-        btnDiv.appendChild(aDemo)
-        let aRepo = document.createElement('a')
-        aRepo.classList.add('btn','btn-sm','btn-outline-secondary')
-        aRepo.href = repoAddress(projects[i].repo)
-        aRepo.innerHTML = 'Code';
-        btnDiv.appendChild(aRepo)
-
-        container.appendChild(div)
+    if (container != null) {
+        for (i=0; i<projects.length; i++) {
+            let div = document.createElement('div')
+            div.classList.add('projects-grid-item')
+            let cardShadowDiv = document.createElement('div')
+            cardShadowDiv.classList.add('card','shadow-sm')
+            div.appendChild(cardShadowDiv)
+            let cardImg = document.createElement('img')
+            cardImg.src = presentImage(projects[i].image)
+    
+            let aImgLink = document.createElement('a')
+            aImgLink.href = demoAddress(projects[i].repo)
+            aImgLink.appendChild(cardImg)
+    
+            cardShadowDiv.appendChild(aImgLink)
+            let cardBody = document.createElement('div')
+            cardBody.classList.add('card-body')
+            cardShadowDiv.appendChild(cardBody)
+            let h4 = document.createElement('h4')
+            h4.classList.add('capitalize')
+            h4.innerHTML = presentName(projects[i].repo)
+            cardBody.appendChild(h4)
+            let p = document.createElement('p')
+            p.classList.add('card-text')
+            p.innerHTML = projects[i].desc
+            cardBody.appendChild(p)
+            let flexDiv = document.createElement('div')
+            flexDiv.classList.add('d-flex','justify-content-between','align-items-center')
+            cardBody.appendChild(flexDiv)
+            let btnDiv = document.createElement('div')
+            btnDiv.classList.add('btn-group')
+            flexDiv.appendChild(btnDiv)
+            let aDemo = document.createElement('a')
+            aDemo.classList.add('btn','btn-sm','btn-outline-secondary')
+            aDemo.href = demoAddress(projects[i].repo)
+            aDemo.innerHTML = 'Demo';
+            btnDiv.appendChild(aDemo)
+            let aRepo = document.createElement('a')
+            aRepo.classList.add('btn','btn-sm','btn-outline-secondary')
+            aRepo.href = repoAddress(projects[i].repo)
+            aRepo.innerHTML = 'Code';
+            btnDiv.appendChild(aRepo)
+    
+            container.appendChild(div)
+        }
     }
+    
 
 
 }
